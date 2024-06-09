080da333cab7d4487d7dc8f7dda97c6329398c53
Lucas Martins Vasconcelos <lucasmartinsvasconcelos@Lucass-MacBook-Pro-2.local>
2024-06-08 23:22:20 -0300

    add orm

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import { app } from './app';
+import { app } from '@/app'
+import { env } from '@/env'
 
-app.listen({
-  host: '0.0.0.0',
-    port: 3000,
-}).then(() => {
-  console.log('HTTP server is running');
-});
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP server is running on port', env.PORT)
+  })
