9f32467d594b37178ca6f89d75b2ab5aeb01a78c
Lucas Martins Vasconcelos <lucasmartinsvasconcelos@Lucass-MacBook-Pro-2.local>
2024-06-15 16:11:13 -0300

    add miss factories

---
+++ src/use-cases/factories/make-search-gyms-use-case.ts
@@ -0,0 +1,10 @@
+import { SearchGymUseCase } from '../search-gym'
+import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
+
+export function makeSearchGymUseCase() {
+  const gymsRepository = new PrismaGymsRepository()
+
+  const useCase = new SearchGymUseCase(gymsRepository)
+
+  return useCase
+}
